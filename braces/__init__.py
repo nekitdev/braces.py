c34d2525470cfe14226525d313af69f59adcae1f
Nikita Tikhonov <nekitdevofficial@gmail.com>
2021-05-06 02:16:29 +0300

    [0.4.0] Change copyright years, add try-except in hook file.

--- braces/__init__.py
+++ braces/__init__.py
@@ -2,9 +2,9 @@
 
 __title__ = "braces"
 __author__ = "nekitdev"
-__copyright__ = "Copyright 2020-2021 nekitdev"
+__copyright__ = "Copyright 2020-present nekitdev"
 __license__ = "MIT"
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 from braces.constants import EXCEPT, LINE_LENGTH, TOKEN  # noqa
 from braces.encoding import decode, encode  # noqa
